f02f02c7a22c1e4bfee922c939c253f4d5e1431a
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-10-12 09:21:21 +0200

    Use the factories in the Bank

--- src/main/java/org/delta/bank/Bank.java
+++ src/main/java/org/delta/bank/Bank.java
@@ -1,28 +1,28 @@
 package org.delta.bank;
 
-import org.delta.bank.account.BaseBankAccount;
-import org.delta.bank.account.InterestAccount;
-import org.delta.bank.account.SavingBankAccount;
-import org.delta.bank.account.StudentBankAccount;
+import org.delta.bank.account.*;
 import org.delta.bank.interest.InterestApplicator;
 import org.delta.bank.print.LogService;
 import org.delta.bank.user.Owner;
+import org.delta.bank.user.OwnerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
-    LogService logService = new LogService();
-    InterestApplicator interestApplicator = new InterestApplicator();
+    private final LogService logService = new LogService();
+    private final InterestApplicator interestApplicator = new InterestApplicator();
+    private final OwnerFactory ownerFactory = new OwnerFactory();
+    private final BankAccountFactory accountFactory = new BankAccountFactory();
 
     public void run() {
-        Owner owner1 = new Owner("John", "Doe");
+        Owner owner1 = ownerFactory.createOwner("John", "Doe");
 
         List<BaseBankAccount> accounts = new ArrayList<>();
 
-        accounts.add(new BaseBankAccount(owner1, "1", 10000));
-        accounts.add(new SavingBankAccount(owner1, "2", 10000));
-        accounts.add(new StudentBankAccount(owner1, "3", 10000));
+        accounts.add(accountFactory.createBaseAccount(owner1, 10000));
+        accounts.add(accountFactory.createSavingAccount(owner1, 10000));
+        accounts.add(accountFactory.createStudentAccount(owner1, 10000));
 
         logService.log("Before:");
         for (BaseBankAccount account : accounts) {
