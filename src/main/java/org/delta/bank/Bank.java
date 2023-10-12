e7d137d6ae570334371304e2ad6b500acd8c108b
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-10-12 10:35:46 +0200

    Use services in bank

--- src/main/java/org/delta/bank/Bank.java
+++ src/main/java/org/delta/bank/Bank.java
@@ -5,6 +5,7 @@ import org.delta.bank.interest.InterestApplicator;
 import org.delta.bank.print.LogService;
 import org.delta.bank.user.Owner;
 import org.delta.bank.user.OwnerFactory;
+import org.delta.bank.user.OwnerService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,24 +13,22 @@ import java.util.List;
 public class Bank {
     private final LogService logService = new LogService();
     private final InterestApplicator interestApplicator = new InterestApplicator();
-    private final OwnerFactory ownerFactory = new OwnerFactory();
-    private final BankAccountFactory accountFactory = new BankAccountFactory();
+    private final AccountService accountService = new AccountService();
+    private final OwnerService ownerService = new OwnerService();
 
     public void run() {
-        Owner owner1 = ownerFactory.createOwner("John", "Doe");
+        Owner john = ownerService.addOwner("John", "Doe");
 
-        List<BaseBankAccount> accounts = new ArrayList<>();
-
-        accounts.add(accountFactory.createBaseAccount(owner1, 10000));
-        accounts.add(accountFactory.createSavingAccount(owner1, 10000));
-        accounts.add(accountFactory.createStudentAccount(owner1, 10000));
+        accountService.addBaseAccount(john, 10000);
+        accountService.addSavingAccount(john, 10000);
+        accountService.addStudentAccount(john, 10000);
 
         logService.log("Before:");
-        for (BaseBankAccount account : accounts) {
+        for (BaseBankAccount account : accountService.getAccounts().values()) {
             logService.logAccountInfo(account);
         }
 
-        for (BaseBankAccount account : accounts) {
+        for (BaseBankAccount account : accountService.getAccounts().values()) {
             if (!(account instanceof InterestAccount)) {
                 continue;
             }
@@ -38,7 +37,7 @@ public class Bank {
         }
 
         logService.log("After:");
-        for (BaseBankAccount account : accounts) {
+        for (BaseBankAccount account : accountService.getAccounts().values()) {
             logService.logAccountInfo(account);
         }
     }
