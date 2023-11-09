98d49e07c4a2e6ce009550ad8b0e95e92eaec55c
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-11-09 08:19:24 +0100

    Bind the Gson generator

--- src/main/java/org/delta/bank/BankInjector.java
+++ src/main/java/org/delta/bank/BankInjector.java
@@ -1,6 +1,8 @@
 package org.delta.bank;
 
 import com.google.inject.AbstractModule;
+import org.delta.bank.json.GsonGenerator;
+import org.delta.bank.json.JsonGenerator;
 import org.delta.bank.print.ConsoleLogger;
 import org.delta.bank.print.Logger;
 
@@ -8,5 +10,6 @@ public class BankInjector extends AbstractModule {
     @Override
     protected void configure() {
         this.bind(Logger.class).to(ConsoleLogger.class);
+        this.bind(JsonGenerator.class).to(GsonGenerator.class);
     }
 }
