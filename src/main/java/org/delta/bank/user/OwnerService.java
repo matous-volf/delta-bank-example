fbeec68596bf7b29a2e262bd60e80e594455124a
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-11-13 08:14:10 +0100

    Use a hashmap in the owner service

--- src/main/java/org/delta/bank/user/OwnerService.java
+++ src/main/java/org/delta/bank/user/OwnerService.java
@@ -4,30 +4,32 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.delta.bank.print.LogService;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Singleton
 public class OwnerService {
-    private List<Owner> owners;
+    private final Map<Integer, Owner> owners;
     @Inject
     private OwnerFactory ownerFactory;
     @Inject
     private LogService logService;
 
     public OwnerService() {
-        owners = new ArrayList<>();
+        owners = new HashMap<>();
     }
 
     public Owner addOwner(String firstName, String lastName) {
         Owner owner = ownerFactory.createOwner(firstName, lastName);
-        storeOwner(owner);
+        return addOwner(owner);
+    }
+
+    public Owner addOwner(Owner owner) {
+        owners.put(owner.getId(), owner);
         return owner;
     }
 
-    private void storeOwner(Owner owner) {
-        owners.add(owner);
+    public Map<Integer, Owner> getOwners() {
+        return owners;
     }
 }
