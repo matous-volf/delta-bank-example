9cf05ec82219d624fa6c906f1373e87061fbca62
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-11-16 10:39:13 +0100

    create the customer notification models

---
+++ src/main/java/org/delta/bank/notification/NotificationData.java
@@ -0,0 +1,5 @@
+package org.delta.bank.notification;
+
+public record NotificationData (String customerName) {
+
+}
---
+++ src/main/java/org/delta/bank/notification/NotifyCustomerEvent.java
@@ -0,0 +1,4 @@
+package org.delta.bank.notification;
+
+public record NotifyCustomerEvent(NotificationData notificationData) {
+}
