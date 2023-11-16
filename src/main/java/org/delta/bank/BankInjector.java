7b18a232b92a79470f74dd2345bfc84b8f3756e3
Matouš Volf <66163112+matous-volf@users.noreply.github.com>
2023-11-16 10:40:11 +0100

    add the event listeners to the `EventBus` in the injector

--- src/main/java/org/delta/bank/BankInjector.java
+++ src/main/java/org/delta/bank/BankInjector.java
@@ -1,6 +1,12 @@
 package org.delta.bank;
 
+import com.google.common.eventbus.EventBus;
 import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
+import com.google.inject.matcher.Matchers;
+import com.google.inject.spi.InjectionListener;
+import com.google.inject.spi.TypeEncounter;
+import com.google.inject.spi.TypeListener;
 import org.delta.bank.json.GsonGenerator;
 import org.delta.bank.json.GsonLoader;
 import org.delta.bank.json.JsonGenerator;
@@ -9,10 +15,23 @@ import org.delta.bank.print.ConsoleLogger;
 import org.delta.bank.print.Logger;
 
 public class BankInjector extends AbstractModule {
+    private final EventBus eventBus = new EventBus("Default EventBus");
+
     @Override
     protected void configure() {
         this.bind(Logger.class).to(ConsoleLogger.class);
         this.bind(JsonGenerator.class).to(GsonGenerator.class);
         this.bind(JsonLoader.class).to(GsonLoader.class);
+
+        this.bind(EventBus.class).toInstance(eventBus);
+        bindListener(Matchers.any(), new TypeListener() {
+            public <I> void hear(TypeLiteral<I> type, TypeEncounter<I> encounter) {
+                encounter.register(new InjectionListener<I>(){
+                    public void afterInjection(I i) {
+                        eventBus.register(i);
+                    }
+                });
+            }
+        });
     }
 }
--- src/main/java/org/delta/bank/Main.java
+++ src/main/java/org/delta/bank/Main.java
@@ -2,12 +2,17 @@ package org.delta.bank;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import org.delta.bank.notification.NotifyCustomerEventListener;
+import org.delta.bank.notification.NotifyNewsletterEventListener;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Injector injector = Guice.createInjector(new BankInjector());
             Bank bank = injector.getInstance(Bank.class);
+
+            injector.getInstance(NotifyCustomerEventListener.class);
+            injector.getInstance(NotifyNewsletterEventListener.class);
 
             bank.run();
         } catch (Exception e) {
